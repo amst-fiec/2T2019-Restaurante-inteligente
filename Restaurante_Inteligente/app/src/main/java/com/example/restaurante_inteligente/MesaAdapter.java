fae9ad4997a521a1d12c599ef2c90cfb35ec8573
Javierab202 <javabena@espol.edu.ec>
2020-01-06 00:18:59 -0500

    Se termina el MesaAdapter, se agg los elementos en estado_restaurante

--- Restaurante_Inteligente/app/src/main/java/com/example/restaurante_inteligente/MesaAdapter.java
+++ Restaurante_Inteligente/app/src/main/java/com/example/restaurante_inteligente/MesaAdapter.java
@@ -1,5 +1,7 @@
 package com.example.restaurante_inteligente;
 
+import android.graphics.Color;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -12,33 +14,55 @@ import java.util.List;
 
 public class MesaAdapter extends RecyclerView.Adapter<MesaAdapter.MesaViewHolder> {
 
-    //private final List<Mesa> items;
+    List<Mesa> items;
+
+    public MesaAdapter(List<Mesa> items) {
+        this.items = items;
+    }
 
     public class MesaViewHolder extends RecyclerView.ViewHolder {
-        //final ImageView imagen;
-        //final TextView id;
-        //final TextView capacidad;
-        //final TextView estado;
+        final ImageView imagen;
+        final TextView id;
+        final TextView capacidad;
+        final TextView estado;
 
         public MesaViewHolder(@NonNull View itemView) {
             super(itemView);
+            imagen = (ImageView) itemView.findViewById(R.id.ImgLogo);
+            id = (TextView) itemView.findViewById(R.id.txtMesaID);
+            capacidad = (TextView) itemView.findViewById(R.id.txtCapacidad);
+            estado = (TextView) itemView.findViewById(R.id.txtEstado);
+        }
+
+        public void asignarDatos(Mesa mesa) {
+            imagen.setImageResource(mesa.getImagen());
+            id.setText("    Mesa: "+mesa.getId());
+            capacidad.setText("     Capacidad:"+mesa.getCapacidad());
+            if(mesa.getEstado().equals(false)){
+                estado.setTextColor(Color.rgb(255,50,50));
+            }else{
+                estado.setTextColor(Color.rgb(50,255,50));
+            }
+            estado.setText("        "+mesa.getEstado());
         }
     }
 
     @NonNull
     @Override
     public MesaAdapter.MesaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_mesa,null,false);
+        return new MesaViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MesaAdapter.MesaViewHolder holder, int position) {
-
+        holder.asignarDatos(items.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return items.size();
     }
 
 }
